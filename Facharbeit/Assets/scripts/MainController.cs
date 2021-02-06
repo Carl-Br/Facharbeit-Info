3c41fdee5cc7ba2f2e7398881744bfe59e3f6d47
Shynex1234 <62182341+Shynex1234@users.noreply.github.com>
2021-02-06 13:29:26 +0100

    tag 2

--- Facharbeit/Assets/scripts/MainController.cs
+++ Facharbeit/Assets/scripts/MainController.cs
@@ -9,13 +9,16 @@ public class MainController : MonoBehaviour
     public GameObject Welt;
     public GameObject Haus;
     public GameObject HotSpot;
+    public GameObject NormalePerson;
     public double Ansteckungsradius= 4.5;
     public int hotSpots = 3;//mindestens 1 (darf nicht null sein)=
-    public int Personen = 30; //zwischen 30 und 2000
+    public int personen = 30; //zwischen 30 und 2000
+    public List<GameObject> personenListe;
 
     // Start is called before the first frame update
     void Start()
     {
+        personenListe= new List<GameObject>();
         erstelleWelt();
     }
 
@@ -29,25 +32,60 @@ public class MainController : MonoBehaviour
     private void erstelleWelt()
     {
         //erstellt die Fläche
-        int Häuser = Personen + hotSpots;
-        double seitenLänge = Math.Sqrt(Personen+hotSpots);
+        int Häuser = personen + hotSpots;
+        double seitenLänge = Math.Sqrt(personen+hotSpots);
         seitenLänge = Math.Ceiling(seitenLänge);//rundet seitenLänge auf
-        Welt.transform.localScale = new Vector3((float)seitenLänge, (float)seitenLänge,1f);//ändere die Welt größe
+        Welt.transform.localScale = new Vector3((float)seitenLänge+1, (float)seitenLänge+1,1f);//ändere die Welt größe (das +1 ist der rand)
 
-        //setzte Häuser
+
+        //legt fest an welchen stellen überall HotSpots sein sollen
+        System.Random rnd = new Random();
+        int randomZahl1 = (int)Math.Round(seitenLänge / 5);
+        int[] HotSpotsArray = new int[hotSpots];
+        for(int i =0;i<hotSpots;i++)
+        {
+            int randomZahl = rnd.Next(1, Häuser);
+            foreach (int j in HotSpotsArray)
+            {
+                if(j== randomZahl)//an dieser Position ist bereits ein HotSpot
+                {
+                    i--;//Zähler soll sich nicht veränder, sondern  nur eine neue Zahl generieren
+                    continue;
+                }
+            }
+            //wenn an der Stelle noch kein HotSpot ist setzte dort einen HotSpot
+            HotSpotsArray[i] = randomZahl;
+        }
+
+
+        //setzte Häuser und personen
         int HotSpotPositions = (int)Math.Round((double)Häuser / hotSpots);
         int gebauteHäuser = 0;
         float xPosition = (((float)seitenLänge / 2) * -1) + 0.5f;
         float yPosition = ((float)seitenLänge / 2) - 0.5f;
 
-        //Wenn man coole Muster mit den Häusern und HotSpots sehen will, dann sollte man die Random Zahlen weg lassen
-        System.Random rnd = new Random();
-        int randomZahl1 = (int)Math.Round(seitenLänge / 5);
-        int randomZahl2 = rnd.Next(3, 14);
         while (Häuser!= gebauteHäuser)
         {
-            
-            Instantiate(Haus, new Vector3(xPosition, yPosition, 1f), new Quaternion(0f, 0f, 0f, 0f));
+            bool setztHotSpot = false;
+            foreach (int i in HotSpotsArray)
+            {
+                if (i == gebauteHäuser)//an dieser Position ist bereits ein HotSpot
+                {
+                    setztHotSpot = true;
+                }
+            }
+
+            if (setztHotSpot == true)//setzte hot stop ohne person
+            {
+                Instantiate(HotSpot, new Vector3(xPosition, yPosition, 1f), new Quaternion(0f, 0f, 0f, 0f));
+            }
+            else//setzt Haus mit person
+            {
+                GameObject gameObjectDerPerson = NormalePerson;
+                personenListe.Add(gameObjectDerPerson);
+                Instantiate(gameObjectDerPerson, new Vector3(xPosition, yPosition, 1f), new Quaternion(0f, 0f, 0f, 0f));
+                Instantiate(Haus, new Vector3(xPosition, yPosition, 1f), new Quaternion(0f, 0f, 0f, 0f));
+            }
             xPosition++;
             if (xPosition > seitenLänge/2)
             {
---
+++ Facharbeit/Assets/scripts/Person.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Person : MonoBehaviour
+{
+    Vector2 Position;
+    bool gehen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        geheZuPosition(new Vector2(transform.position.x, transform.position.y));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gehen == true)
+        {
+            transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), Position, 0.95f * Time.deltaTime);
+            if(new Vector2(transform.position.x , transform.position.y)== Position)
+            {
+                gehen = false;
+            }
+        }
+    }
+
+    private void geheZuPosition(Vector2 position)
+    {
+        System.Random rnd = new System.Random();
+        Position = new Vector2(transform.position.x - 0.5f, transform.position.y - 0.5f);
+        gehen = true;
+        
+    }
+    
+
+}
